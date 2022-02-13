642a5ae9c7615b9f7054fa740634d54a83a9981d
Kristoffer Klokker <kristoffer.klokker@gmail.com>
2022-02-13 14:13:02 -0800

    Week 6 exercises

---
+++ Exercises/javaPrograms/binarySearch/BinarySearch.java
@@ -0,0 +1,22 @@
+public class BinarySearch {
+    public static void main(String[] args){
+        Integer[] test = new Integer[]{1,2,3,4,5,6,7};
+        for(Integer i: test){
+            if(test[binarySearch(test, i)] != i)
+                System.out.println("Error: " + i);
+        }
+    }
+
+    public static <T extends Comparable<T>> int binarySearch(T[] list, T value) {
+        int i = list.length/2;
+        int j = list.length/2 + list.length%2;
+        while(j != 0 && list[i] != value) {
+            j = j/2 + j%2;
+            if(list[i].compareTo(value) < 0)
+                i = i + j;
+            else
+                i = i - j;
+        }
+        return i;
+    }
+}
